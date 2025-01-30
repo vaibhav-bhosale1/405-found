b4175d5b814b66cfd0cc59f40eeece7130146652
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2025-01-30 15:17:52 +0530

    added ui

--- community-building-and-event-management-platform/src/app/page.js
+++ community-building-and-event-management-platform/src/app/page.js
@@ -1,6 +1,6 @@
-import Header from "@/components/ui/custom/Header";
-import Hero from "@/components/ui/custom/Hero";
-import Image from "next/image";
+
+import Header from "../components/ui/custom/Header";
+import Hero from "../components/ui/custom/Hero";
 
 
 export default function Home() {
